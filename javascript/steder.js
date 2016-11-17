01c0c76dc2cb9bd20f1c229dd3ca2030f648bc71
L4443 <eirik.leikvoll@gmail.com>
2016-11-17 12:46:06 +0100

    Kart relaterte filer

---
+++ javascript/steder.js
@@ -0,0 +1,20 @@
+var locations = [
+    {navn: 'Vending', location: L.latLng(60.473889, 6.075278)},
+    []
+];
+
+var hogabu = {
+    location: L.latLng(60.535, 5.924722)
+};
+
+var alexgrieg = {
+    location: L.latLng(60.536944, 6.079167)
+};
+
+var gullhorgabu = {
+    location: L.latLng(60.500833, 5.960278)
+};
+
+var vending = {
+    location: L.latLng(60.473889, 6.075278)
+}
