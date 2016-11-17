3f89c053eb37e6cb037706ee7d17674a0be200ed
L4443 <eirik.leikvoll@gmail.com>
2016-11-17 12:53:03 +0100

    esg<f

--- javascript/map.js
+++ javascript/map.js
@@ -1,8 +1,15 @@
-var map = L.map('map').setView([60.535, 5.924722], 10);
+var map = L.map('map-div').setView([60.535, 5.924722], 10);
 
-L.tileLayer('http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=norges_grunnkart&zoom={z}&x={x}&y={y}', {
+/*L.tileLayer('http://opencache.statkart.no/gatekeeper/gk/gk.open_gmaps?layers=norges_grunnkart&zoom={z}&x={x}&y={y}', {
     attribution: '<a href="http://www.kartverket.no/">Kartverket</a>'
-}).addTo(map);
+}).addTo(map);*/
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'your.mapbox.project.id',
+    accessToken: 'your.mapbox.public.access.token'
+}).addTo(mymap);
 
 L.tileLayer('http://mt3.turistforeningen.no/prod/trail_summer/{z}/{x}/{y}.png', {
     attribution: '<a href="https://www.dnt.no/">DNT</a>'
