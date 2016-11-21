935ab2b95ec3bb24167cbd5bb3bcb26c52628e23
askbk <askbk@protonmail.com>
2016-11-21 08:56:07 +0100

    må endre datatype på hytter til array. fikset en del ting

---
+++ javascript/hyttevelger.js
@@ -0,0 +1,6 @@
+function finnHytter() { //funksjon for å generere liste over hytter
+  var areaSelect = document.getElementById("areaSelect");
+  var sted = areaSelect.options[areaSelect.selectedIndex].value;
+  console.log(sted);
+  
+}
--- javascript/steder.js
+++ javascript/steder.js
@@ -3,27 +3,36 @@
     []
 ];*/ //kommentert ut pga eirik ikke har sagt hva variabelen er til.
 
-//objekter må inneholde: koordinater, bilde av hytte, beskrivelse av hyttefasiliteter og området rundt.
-var hogabu = {
+//objekter må inneholde: koordinater, sted (bergsdalen), bilde av hytte, beskrivelse av hyttefasiliteter og området rundt.
+//datastruktur på liste over hytter: hytteliste[hytte[egenskaper]]. elementene til hytte er [sted, location, bilde, beskrivelse]
+
+//må gjøre om alle objekter til array
+
+/*var hogabu = {
     location: L.latLng(60.535, 5.924722),
+    sted: "bergsdalen",
     picture: "img/hogabu.jpg",
     description: "beskrivelse av hogabu"
 };
 
 var alexgrieg = {
     location: L.latLng(60.536944, 6.079167),
+    sted: "bergsdalen",
     picture: "img/alexgrieg.jpg",
     description: "beskrivelse av alexgrieg"
 };
 
 var gullhorgabu = {
     location: L.latLng(60.500833, 5.960278),
+    sted: "bergsdalen",
     picture: "img/gullhorgabu.jpg",
     description: "beskrivelse av gullhorgabu"
 };
 
 var vending = {
     location: L.latLng(60.473889, 6.075278),
+    sted: "bergsdalen",
     picture: "img/vending.jpg",
     description: "beskrivelse av vending"
 }
+*/
