2d80c1e976e49a943baa91526ce77b3958bb6a83
askbk <askbk@protonmail.com>
2016-11-21 08:20:42 +0100

    fixet mer på infobox, steder.js

--- javascript/steder.js
+++ javascript/steder.js
@@ -1,20 +1,29 @@
-var locations = [
+/*var locations = [
     {navn: 'Vending', location: L.latLng(60.473889, 6.075278)},
     []
-];
+];*/ //kommentert ut pga eirik ikke har sagt hva variabelen er til.
 
+//objekter må inneholde: koordinater, bilde av hytte, beskrivelse av hyttefasiliteter og området rundt.
 var hogabu = {
-    location: L.latLng(60.535, 5.924722)
+    location: L.latLng(60.535, 5.924722),
+    picture: "img/hogabu.jpg",
+    description: "beskrivelse av hogabu"
 };
 
 var alexgrieg = {
-    location: L.latLng(60.536944, 6.079167)
+    location: L.latLng(60.536944, 6.079167),
+    picture: "img/alexgrieg.jpg",
+    description: "beskrivelse av alexgrieg"
 };
 
 var gullhorgabu = {
-    location: L.latLng(60.500833, 5.960278)
+    location: L.latLng(60.500833, 5.960278),
+    picture: "img/gullhorgabu.jpg",
+    description: "beskrivelse av gullhorgabu"
 };
 
 var vending = {
-    location: L.latLng(60.473889, 6.075278)
+    location: L.latLng(60.473889, 6.075278),
+    picture: "img/vending.jpg",
+    description: "beskrivelse av vending"
 }
