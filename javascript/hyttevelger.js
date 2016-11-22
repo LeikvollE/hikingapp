1a0cdb4fa0c01e19312932e71e5b6b4698ed5cce
L4443 <eirik.leikvoll@gmail.com>
2016-11-22 20:54:37 +0100

    Mulighet for valg av 2 hytter

--- javascript/hyttevelger.js
+++ javascript/hyttevelger.js
@@ -1,41 +1,56 @@
 //Div-en som skal vise informasjon om hyttene
 var hyttediv = document.getElementById("listemedhytter");
+var valgteHytter = [];
 
 function finnHytter() { //funksjon for å generere liste over hytter
-  var areaSelect = document.getElementById("areaSelect");         //dropdown for turområde
-  var sted = areaSelect.options[areaSelect.selectedIndex].value;  //henter valgt område
-  var hytteliste = [];                                            //variabel for hytter i valgt område
+    var areaSelect = document.getElementById("areaSelect"); //dropdown for turområde
+    var sted = areaSelect.options[areaSelect.selectedIndex].value; //henter valgt område
+    var hytteliste = []; //variabel for hytter i valgt område
 
-  for (var i = 0; i < hytter.length; i++) {                       //går gjennom alle hyttene til turlaget og legger til de som ligger i valgt område i hyttelisten.
-    var j = 0;
-    if (hytter[i][1] === sted) {
-      hytteliste.push(hytter[i]);
+    for (var i = 0; i < hytter.length; i++) { //går gjennom alle hyttene til turlaget og legger til de som ligger i valgt område i hyttelisten.
+        var j = 0;
+        if (hytter[i][1] === sted) {
+            hytteliste.push(hytter[i]);
+        }
     }
-  }
-  skrivUtHytter(hytteliste);
+    skrivUtHytter(hytteliste);
 }
 
 //Midlertidig funksjon for å skrive ut hytter på siden
-function skrivUtHytter(liste){
+function skrivUtHytter(liste) {
     hyttediv.innerHTML = "";
-    for(var i = 0; i < liste.length; i++){
+    for (var i = 0; i < liste.length; i++) {
         //for(var j = 0; j < liste[i].length; j++){
-            //Skriver foreløpig bare ut navnet på hytten, formatering må gjøres f.eks. Stor bokstav i navnet til hytten
-            hyttediv.innerHTML += '<div class="hytter" onclick="hytteInfo(' + liste[i][5] +')">' + liste[i][2] + '</div>';
+        //Skriver foreløpig bare ut navnet på hytten, formatering må gjøres f.eks. Stor bokstav i navnet til hytten
+        hyttediv.innerHTML += '<div class="hytter" onclick="hytteInfo(' + liste[i][5] + ')">' + liste[i][2] + '</div>';
         //}
     }
 }
 
 //funksjon for å skrive ut info om valgt hytte. må vite om dette er valgt hytte nr 1 eller nr 2.
-function hytteInfo(hytteId){
-  var infohytte1 = document.getElementById("infohytte1");
-  var infohytte2 = document.getElementById("infohytte2");
+function hytteInfo(hytteId) {
+    var infohytte1 = document.getElementById("infohytte1");
+    var infohytte2 = document.getElementById("infohytte2");
 
-  for (var i = 0; i < hytter.length; i++) {                       //skriver ut i infoboksen. kun hytte 1 foreløpig.
-    if (hytter[i][5] === hytteId) {
-      infohytte1.innerHTML = "<center><b>" + hytter[i][2] + "</b></center><br>" + "<center>" + hytter[i][4] + "</center>";
-        infohytte1.innerHTML += "<center><img src=" + hytter[i][3] + " width='90%'></center>";
-        map.setView(hytter[i][0], 15);
+    for (var i = 0; i < hytter.length; i++) { //skriver ut i infoboksen. kun hytte 1 foreløpig.
+        if (hytter[i][5] === hytteId) {
+            
+            if(valgteHytter.length === 2){
+                valgteHytter[0] = valgteHytter[1];
+                valgteHytter.pop();
+                valgteHytter.push(hytter[i])
+            } else {
+                valgteHytter.push(hytter[i]);
+            }
+            
+            
+            
+            /*infohytte1.innerHTML = "<center><b>" + hytter[i][2] + "</b></center><br>" + "<center>" + hytter[i][4] + "</center>";
+            infohytte1.innerHTML += "<center><img src=" + hytter[i][3] + " width='90%'></center>";
+            map.setView(hytter[i][0], 15);*/
+        }
     }
-  }
+    
+    infohytte1.innerHTML = "<center><b>" + valgteHytter[0][2] + "</b></center><br>" + "<center>" + valgteHytter[0][4] + "</center>";
+    infohytte2.innerHTML = "<center><b>" + valgteHytter[1][2] + "</b></center><br>" + "<center>" + valgteHytter[1][4] + "</center>";
 }
