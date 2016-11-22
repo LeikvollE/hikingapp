a4cdc6e6db01b51f2ff7819e30bbe5d2137bfddc
askbk <askbk@protonmail.com>
2016-11-22 22:01:12 +0100

    la til funksjon for å velge kun én hytte ;) eirik blodøks

--- javascript/hyttevelger.js
+++ javascript/hyttevelger.js
@@ -34,7 +34,7 @@ function hytteInfo(hytteId) {
 
     for (var i = 0; i < hytter.length; i++) { //skriver ut i infoboksen. kun hytte 1 foreløpig.
         if (hytter[i][5] === hytteId) {
-            
+
             if(valgteHytter.length === 2){
                 valgteHytter[0] = valgteHytter[1];
                 valgteHytter.pop();
@@ -42,22 +42,33 @@ function hytteInfo(hytteId) {
             } else {
                 valgteHytter.push(hytter[i]);
             }
-            
-            
-            
+
+
+
             /*infohytte1.innerHTML = "<center><b>" + hytter[i][2] + "</b></center><br>" + "<center>" + hytter[i][4] + "</center>";
             infohytte1.innerHTML += "<center><img src=" + hytter[i][3] + " width='90%'></center>";
             map.setView(hytter[i][0], 15);*/
         }
     }
-    
+
     infohytte1.innerHTML = "<center><b>" + valgteHytter[0][2] + "</b></center><br>" + "<center>" + valgteHytter[0][4] + "</center>";
-    infohytte2.innerHTML = "<center><b>" + valgteHytter[1][2] + "</b></center><br>" + "<center>" + valgteHytter[1][4] + "</center>";
-    
-    if(valgteHytter.length === 2){
+    if (valgteHytter.length === 2) {
+      infohytte2.innerHTML = "<center><b>" + valgteHytter[1][2] + "</b></center><br>" + "<center>" + valgteHytter[1][4] + "</center>";
+    }
+
+    if(valgteHytter.length === 2){    //tegner inn rute på kart dersom 2 hytter er valgt
         routeControl.setWaypoints([
             valgteHytter[0][0],
             valgteHytter[1][0]
         ]);
+    }else {
+      for (var i = 0; i < startSteder.length; i++) {
+        if (startSteder[i][2] === hytteId) {
+          routeControl.setWaypoints([
+              valgteHytter[0][0],
+              startSteder[i][0]
+          ]);
+        }
+      }
     }
 }
--- javascript/steder.js
+++ javascript/steder.js
@@ -5,16 +5,25 @@
 
 //objekter må inneholde: koordinater, sted (bergsdalen), bilde av hytte, beskrivelse av hyttefasiliteter og området rundt.
 //datastruktur på liste over hytter: hytteliste[hytte[karfunksjon(koordinater), område, hyttenavn, bildested, beskrivelse, ID]].
-
 var hytter = [
   [L.latLng(60.535, 5.924722), "bergsdalen", "Høgabu", "img/hogabu.jpg", "beskrivelse av hogabu", 0],
   [L.latLng(60.536944, 6.079167), "bergsdalen", "Alexander Grieghytten", "img/alexgrieg.jpg", "beskrivelse av alexgrieg", 1],
   [L.latLng(60.500833, 5.960278), "bergsdalen", "Gullhorgabu", "img/gullhorgabu.jpg", "beskrivelse av gullhorgabu", 2],
   [L.latLng(60.473889, 6.075278), "bergsdalen", "Vending", "img/vending.jpg", "beskrivelse av vending", 3],
-  [L.latLng(60.473889, 6.075278), "dal", "vending", "img/vending.jpg", "kopi for å teste hyttevelger", 4],
-  [L.latLng(60.514602, 6.201278), "bergsdalen", "Kiellandbu", "img/kiellandbu.jpg", "beskrivelse", 5],
-  [L.latLng(60.465862, 6.125688), "bergsdalen", "Breidablik", "img/breidablik.jpg", "beskrivelse", 6]
-]; //bruker annet sted enn bergsdalen på siste element
+  [L.latLng(60.514602, 6.201278), "bergsdalen", "Kiellandbu", "img/kiellandbu.jpg", "beskrivelse", 4],
+  [L.latLng(60.465862, 6.125688), "bergsdalen", "Breidablik", "img/breidablik.jpg", "beskrivelse", 5]
+];
+
+//struktur på startsteder: startSteder[startsted[kartfunksjon(koordinater), beskrivelse, ID]]
+//hver hytte har ett designert startsted dersom man kun har valgt 1 hytte i hyttevelgeren
+var startSteder = [
+  [L.latLng(60.571399, 5.949952), "lid i bergsdalen", 0],
+  [L.latLng(60.534741, 6.074092), "parkeringsplass ved hovedveien", 1],
+  [L.latLng(60.544582, 6.030346), "småbrekkene i bergsdalen", 2],
+  [L.latLng(60.534741, 6.074092), "fra hovedveien", 3],
+  [L.latLng(60.560545, 6.208481), "hodnaberg i enden av hamlagrøvatnet", 4],
+  [L.latLng(60.534741, 6.074092), "fra hovedveien", 5]
+];
 
 /*var hogabu = {
     location: L.latLng(60.535, 5.924722),
