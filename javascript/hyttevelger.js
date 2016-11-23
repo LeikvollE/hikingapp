f7edfe7aa0ce19041759c8e8b2a04ef89ce4f0e3
askbk <askbk@protonmail.com>
2016-11-23 18:52:52 +0100

    pyntet på hyttevelger.js og la til flere kommentarer.

--- javascript/hyttevelger.js
+++ javascript/hyttevelger.js
@@ -7,8 +7,7 @@ function finnHytter() { //funksjon for å generere liste over hytter
     var sted = areaSelect.options[areaSelect.selectedIndex].value; //henter valgt område
     var hytteliste = []; //variabel for hytter i valgt område
 
-    for (var i = 0; i < hytter.length; i++) { //går gjennom alle hyttene til turlaget og legger til de som ligger i valgt område i hyttelisten.
-        var j = 0;
+    for (var i = 0; i < hytter.length; i++) { //går gjennom alle hyttene til turlaget og legger til de som ligger i valgt område til hyttelisten.
         if (hytter[i][1] === sted) {
             hytteliste.push(hytter[i]);
         }
@@ -18,35 +17,31 @@ function finnHytter() { //funksjon for å generere liste over hytter
 
 //Midlertidig funksjon for å skrive ut hytter på siden
 function skrivUtHytter(liste) {
-    hyttediv.innerHTML = "";
-    for (var i = 0; i < liste.length; i++) {
-        //for(var j = 0; j < liste[i].length; j++){
-        //Skriver foreløpig bare ut navnet på hytten, formatering må gjøres f.eks. Stor bokstav i navnet til hytten
+    hyttediv.innerHTML = "";  //tømmer innhold i hyttediv
+
+    for (var i = 0; i < liste.length; i++) {  //skriver ut alle elementer i liste over hytter
         hyttediv.innerHTML += '<div class="hytter" onclick="hytteInfo(' + liste[i][5] + ')" id="hytte' + liste[i][5] + '"><img src="' + liste[i][3] + '"><b>' + liste[i][2] + '</b></div>';
-        //}
     }
 }
 
-//funksjon for å skrive ut info om valgt hytte. må vite om dette er valgt hytte nr 1 eller nr 2.
+//funksjon for å skrive ut info om valgt hytte.
 function hytteInfo(hytteId) {
-    var infohytte1 = document.getElementById("infohytte1");
+    var infohytte1 = document.getElementById("infohytte1");         //elementet til info om hytte 1 og 2
     var infohytte2 = document.getElementById("infohytte2");
     var hytteElement = document.getElementById("hytte" + hytteId);
 
     for (var i = 0; i < hytter.length; i++) { //skriver ut i infoboksen. kun hytte 1 foreløpig.
         if (hytter[i][5] === hytteId) {
-
-            if(erValgt(hytter[i])){
-                //TODO Kode for å fjerne elementet som ble trykket på
+            if(erValgt(hytter[i])){  //sjekker om hytte[i] er valgt fra før. fjerner i det tilfellet klassen "valgt" for å endre utseendet.
                 hytteElement.className = hytteElement.className.replace(/\bvalgt\b/,'');
-                if (hytter[i][5]===valgteHytter[0][5]) {
+                if (hytter[i][5]===valgteHytter[0][5]) {  //
                   valgteHytter.splice(0,1);
                 }else {
                   valgteHytter.splice(1,1);
                 }
                 break;
             }else {
-              hytteElement.className += " valgt"; //legger til klassen "valgt" på hytter som er valgt
+              hytteElement.className += " valgt"; //legger til klassen "valgt" på hytter som er valgt for å endre utseendet.
             }
 
             if(valgteHytter.length === 2){          //dersom 2 hytter er valgt fra før, fjernes den siste fra arrayet. ellers blir den nye hytta lagt til i arrayet
@@ -57,17 +52,12 @@ function hytteInfo(hytteId) {
             } else {
                 valgteHytter.push(hytter[i]);
             }
-
             break;
-
-            /*infohytte1.innerHTML = "<center><b>" + hytter[i][2] + "</b></center><br>" + "<center>" + hytter[i][4] + "</center>";
-            infohytte1.innerHTML += "<center><img src=" + hytter[i][3] + " width='90%'></center>";
-            map.setView(hytter[i][0], 15);*/
         }
     }
 
-    infohytte1.innerHTML = "<center><b>" + valgteHytter[0][2] + "</b></center><br>" + "<center>" + valgteHytter[0][4] + "</center>"; //legger til
-    if (valgteHytter.length === 2) {
+    infohytte1.innerHTML = "<center><b>" + valgteHytter[0][2] + "</b></center><br>" + "<center>" + valgteHytter[0][4] + "</center>"; //legger til info i infoboksen
+    if (valgteHytter.length === 2) {    //dersom 2 hytter er valgt blir info om begge lagt til i infoboksen.
       infohytte2.innerHTML = "<center><b>" + valgteHytter[1][2] + "</b></center><br>" + "<center>" + valgteHytter[1][4] + "</center>";
     }
 
@@ -90,10 +80,8 @@ function hytteInfo(hytteId) {
 
 function erValgt(hytte) {                 //sjekker om hytte er valgt
     var ant = valgteHytter.length;
-
     for(var i = 0; i < ant; i++) {
         if (valgteHytter[i][5] === hytte[5]) return true;
     }
-
     return false;
 }
